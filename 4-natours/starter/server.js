8c6d4fdfb6f98d6c0e81a82c6c0a1bb132d6c167
Zhafri <mzhafri89@gmail.com>
2022-12-07 11:24:54 +0800

    init db

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,33 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 //always load env config before app module is loaded.
 dotenv.config({ path: './config.env' });
 
+//init database
+const {
+  env: { DB_URL, DB_USER, DB_PASSWORD },
+} = process;
+
+const databaseConnectionString = DB_URL.replace('<USER>', DB_USER).replace(
+  '<PASSWORD>',
+  DB_PASSWORD
+);
+
+mongoose
+  .connect(databaseConnectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.info('Database connected.');
+  });
+
+//init app
 const app = require('./app');
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
