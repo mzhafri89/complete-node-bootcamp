76e006d0cf06341b1a549e8efc75bf08a15d2804
Zhafri <mzhafri89@gmail.com>
2022-12-23 17:34:36 +0800

    Tours api features.

--- 4-natours/starter/controllers/tour.js
+++ 4-natours/starter/controllers/tour.js
@@ -1,8 +1,65 @@
 const Tour = require('../models/tour');
 
-exports.getAllTour = async (_, res) => {
+exports.getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //mongoose method
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(duration)
+    //   .where('difficulty')
+    //   .equals(difficulty);
+
+    //build query
+    //filtering
+    const queryParams = { ...req.query };
+    const paginationQueryParams = ['page', 'sort', 'limit', 'fields'];
+    paginationQueryParams.forEach((param) => delete queryParams[param]);
+
+    //advance filtering - express auto parse query params of fields[gte] = 10 to { fields: { gte: 10 } }
+    let queryParamsString = JSON.stringify(queryParams);
+    queryParamsString = queryParamsString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let databaseQuery = Tour.find(JSON.parse(queryParamsString));
+
+    //sorting - the sort by token would affect sorting from left to right
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      databaseQuery = databaseQuery.sort(sortBy);
+    } else {
+      databaseQuery = databaseQuery.sort('createdAt');
+    }
+
+    //field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+
+      databaseQuery = databaseQuery.select(fields);
+    } else {
+      databaseQuery = databaseQuery.select('-__v');
+    }
+
+    //pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 5;
+    const skip = (page - 1) * limit;
+
+    databaseQuery = databaseQuery.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const count = await Tour.countDocuments();
+      if (skip >= count) {
+        throw new Error('No documents exist in the page');
+      }
+    }
+
+    //execute query
+    const tours = await databaseQuery;
+
+    //send response
     res.status(200).json({
       //format using JSend standard
       status: 'success',
@@ -96,6 +153,14 @@ exports.deleteTour = async (req, res) => {
       message: e.message,
     });
   }
+};
+
+exports.aliasTopRated = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
 };
 
 exports.checkId = (req, res, next, value) => {
--- 4-natours/starter/routes/tour.js
+++ 4-natours/starter/routes/tour.js
@@ -5,13 +5,13 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  checkId,
-  checkBody,
+  aliasTopRated,
 } = require('../controllers/tour');
 
 const router = express.Router();
 
 //param middleware would only work for this route
+router.route('/top-rated').get(aliasTopRated, getAllTour); //route aliasing
 router.route(`/`).get(getAllTour).post(createTour);
 router.route(`/:id`).get(getTour).patch(updateTour).delete(deleteTour);
 
