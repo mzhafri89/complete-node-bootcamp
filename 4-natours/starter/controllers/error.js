47f4189ae81f91606cf7f5f0cebab455bead6398
Zhafri <mzhafri89@gmail.com>
2022-12-28 18:40:05 +0800

    handle dev and prod error.

--- 4-natours/starter/controllers/error.js
+++ 4-natours/starter/controllers/error.js
@@ -1,9 +1,35 @@
+const handleDevError = (error, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack,
+    error,
+  });
+};
+
+const handleError = (error, res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  } else {
+    console.error(error);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong.',
+    });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    handleDevError(error, res);
+  } else {
+    handleError(error, res);
+  }
 };
