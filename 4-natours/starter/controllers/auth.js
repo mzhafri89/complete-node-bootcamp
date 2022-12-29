d1f831d96251b5ed3e8a3a556ac5ae48bea69c7c
Zhafri <mzhafri89@gmail.com>
2022-12-29 16:32:49 +0800

    fix user register.

--- 4-natours/starter/controllers/auth.js
+++ 4-natours/starter/controllers/auth.js
@@ -2,7 +2,13 @@ const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
 exports.register = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  //manually specified the field so that we only register user with current fields
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
