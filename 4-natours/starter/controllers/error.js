755b68991e01d815b20423829115322f8b1c2b63
Zhafri <mzhafri89@gmail.com>
2022-12-29 12:12:40 +0800

    handle bad tour id.

--- 4-natours/starter/controllers/error.js
+++ 4-natours/starter/controllers/error.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const handleDevError = (error, res) => {
   res.status(error.statusCode).json({
     status: error.status,
@@ -23,6 +25,12 @@ const handleError = (error, res) => {
   }
 };
 
+const handleDatabaseCastError = (error) => {
+  const message = `Invalid ${error.path}: ${error.value}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
@@ -30,6 +38,13 @@ module.exports = (error, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     handleDevError(error, res);
   } else {
-    handleError(error, res);
+    let errorCopy;
+
+    //convert db error to operational error
+    if (error.name === 'CastError') {
+      errorCopy = handleDatabaseCastError(error);
+    }
+
+    handleError(errorCopy, res);
   }
 };
